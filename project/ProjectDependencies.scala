1f5ea6c7baff661c0ecd306c6afce03892dad2bd
Dan Dixey <Dan.Dixey@woodmac.com>
2019-10-28 08:41:53 +0000

    Minor refactor

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -4,7 +4,6 @@ object ProjectDependencies extends {
 
   private[this] object Version {
     val argonPass        = "2.5"
-    val doobieVersion    = "0.7.1"
     val profigVersion    = "2.3.4"
     val scalaTestVersion = "3.0.5"
   }
@@ -12,11 +11,10 @@ object ProjectDependencies extends {
   def apply(): Seq[ModuleID] = Seq(
     // Project dependencies
     "de.mkammerer"  % "argon2-jvm"   % Version.argonPass,
-    "org.tpolecat"  %% "doobie-core" % Version.doobieVersion,
     "com.outr"      %% "profig"      % Version.profigVersion,
     "org.scalatest" %% "scalatest"   % Version.scalaTestVersion % "it, test",
     // Compiler Plugins
     compilerPlugin("org.spire-math" %% "kind-projector"     % "0.9.4"),
-    compilerPlugin("com.olegpy"     %% "better-monadic-for" % "0.3.0-M4")
+    compilerPlugin("com.olegpy"     %% "better-monadic-for" % "0.3.1")
   )
 }
