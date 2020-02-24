24c19be2fb35b2310fe46af043010c77febfab9a
Dan Dixey <daniel.dixey@itv.com>
2020-02-24 22:59:56 +0000

    Feature/update dependencies (#2)
    * Update dependencies
    
    * Bump scalafmt 2.4.2
    
    * Bump libraries
    
    * Update ScalaOptions

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -1,11 +1,11 @@
-import sbt.{ ModuleID, compilerPlugin, _ }
+import sbt._
 
 object ProjectDependencies extends {
 
   private[this] object Version {
-    val argonPass        = "2.5"
-    val profigVersion    = "2.3.4"
-    val scalaTestVersion = "3.0.5"
+    val argonPass        = "2.6"
+    val profigVersion    = "2.3.7"
+    val scalaTestVersion = "3.1.1"
   }
 
   def apply(): Seq[ModuleID] = Seq(
@@ -14,7 +14,7 @@ object ProjectDependencies extends {
     "com.outr"      %% "profig"    % Version.profigVersion,
     "org.scalatest" %% "scalatest" % Version.scalaTestVersion % "it, test",
     // Compiler Plugins
-    compilerPlugin("org.spire-math" %% "kind-projector"     % "0.9.4"),
-    compilerPlugin("com.olegpy"     %% "better-monadic-for" % "0.3.1")
+    compilerPlugin("org.typelevel" %% "kind-projector"     % "0.11.0" cross CrossVersion.full),
+    compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1")
   )
 }
